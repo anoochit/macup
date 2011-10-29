b43a21d89883dec5087cbde3f31b806c04ba5e68
anoochit <anoochit@b490e95a-4aa8-66d5-0fa3-0c836cbd8de2>
2011-10-29 13:35:30 +0000

    test google chart api in dashboard

---
+++ www/module/dashboard/dashboard.js
@@ -0,0 +1,19 @@
+     google.load('visualization', '1', {packages:['gauge']});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Label');
+        data.addColumn('number', 'Value');
+        data.addRows(3);
+        data.setValue(0, 0, 'Memory');
+        data.setValue(0, 1, 80);
+        data.setValue(1, 0, 'CPU');
+        data.setValue(1, 1, 55);
+        data.setValue(2, 0, 'Network');
+        data.setValue(2, 1, 68);
+
+        var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+        var options = {width: 600, height: 220, redFrom: 90, redTo: 100,
+            yellowFrom:75, yellowTo: 90, minorTicks: 5};
+        chart.draw(data, options);
+      }
--- www/module/dashboard/dashboard.php
+++ www/module/dashboard/dashboard.php
@@ -4,3 +4,12 @@
 	$obmc->isAuthen();
  
 ?>
+<table id="dashboard-body">
+<tr>
+	<td><h1>Gauge</h1></td>
+</tr>
+<tr>
+	<td><div id='chart_div'></div></td>
+</tr>
+</table>
+
