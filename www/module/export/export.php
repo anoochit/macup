0a60c6ab4025db6369f844fa41ec5ce1ae278239
anoochit <anoochit@b490e95a-4aa8-66d5-0fa3-0c836cbd8de2>
2011-11-01 23:46:02 +0000

    add export, report mobule

--- www/module/export/export.php
+++ www/module/export/export.php
@@ -4,3 +4,8 @@
 	$obmc->isAuthen();
  
 ?>
+<table id="dashboard-body">
+<tr>
+	<td><input type="button" value="Download CVS Text File" class="submit" onClick="location.href='/export/csv'"></td>
+</tr>
+</table>
