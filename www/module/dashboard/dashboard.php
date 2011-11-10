5eed10dcf82dd5add1ac58d84bfaad628c88b23e
anoochit <anoochit@b490e95a-4aa8-66d5-0fa3-0c836cbd8de2>
2011-11-10 10:10:45 +0000

    mod dashboard and coupon bind with user for statistic checking

--- www/module/dashboard/dashboard.php
+++ www/module/dashboard/dashboard.php
@@ -11,10 +11,7 @@
 ?>
 <table id="dashboard-body">
 <tr>
-	<td><h3>Your Info : <?php echo $res->login; ?></h3></td>
-</tr>
-<tr>
-	<td><h3>Amount = <?php echo count($total); ?></h3></td>
+	<td><h3>Your Info = <?php echo $res->login; ?>, amount = <?php echo count($total); ?></h3></td>
 </tr>
 </table>
 <table id="dashboard-body">
@@ -24,16 +21,23 @@
 <?php 
 
 	$resuser=$obmc->getUser();
+	$total=0;
 	foreach ($resuser as $user) {
 		$user_total=$obmc->getUserStatByID($user->id);
+			$amount=count($user_total);
+			$total+=$amount;
 ?>
 <tr>
 	<td><?php echo $user->login; ?></td> 
-	<td>= <?php echo count($user_total); ?></td>
+	<td>= <?php echo $amount; ?></td>
 </tr>
 
 <?php 
 	}
 	
 ?>
+<tr>
+	<td><h4>Total</h4></td> 
+	<td><h4>= <?php echo $total; ?></h4></td>
+</tr>
 </table>
